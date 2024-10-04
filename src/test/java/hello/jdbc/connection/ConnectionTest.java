289f2eb161ae9c7367ab9eda18b4e28ca6f8d858
chaewonkim <kimlove9182@gmail.com>
2024-10-05 00:00:09 +0900

    DataSource 예제2 - 커넥션 풀

--- src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package hello.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -29,9 +30,24 @@ public class ConnectionTest {
         useDataSource(dataSource);
     }
 
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        // 커넥션 풀링
+        // HikariDataSource는 백그라운드 쓰레드에서 커넥션 풀을 준비하고, 필요할 때 커넥션을 생성
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USER);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10); //default 10개
+        dataSource.setPoolName("MyPool");
+
+        useDataSource(dataSource);
+        Thread.sleep(1000);
+    }
+
     private void useDataSource(DataSource dataSource) throws SQLException {
-        Connection con1 = dataSource.getConnection();
-        Connection con2 = dataSource.getConnection();
+        Connection con1 = dataSource.getConnection(); //Pool 1개
+        Connection con2 = dataSource.getConnection(); //획득 Pool 한개를 획득
         log.info("connection={}, class={}", con1, con1.getClass());
         log.info("connection={}, class={}", con2, con2.getClass());
     }
